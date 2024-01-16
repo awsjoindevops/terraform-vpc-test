1682365ef977cbad938a2898346bf3ea9555f29f
awsdevopsonline <punnam.elite@gmail.com>
2024-01-16 11:13:07 +0530

    Git commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+module "roboshop" {
+    source = "../terraform-aws-vpc"
+    project_name = var.project_name
+    environment = var.environment
+    common_tags = var.common_tags
+    vpc_tags = var.vpc_tags
+
+    #Public subnet
+    public_subnet_cidr = var.public_subnet_cidr
+    #Private Subnet
+    private_subnet_cidr = var.private_subnet_cidr
+    #Database Subnet
+    database_subnet_cidr = var.database_subnet_cidr
+        
+    is_peering_required = var.is_peering_required
+
+    
+    
+    }
