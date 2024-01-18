207cf042c41ea03febc64fa8ba182626f36487e9
awsdevopsonline <punnam.elite@gmail.com>
2024-01-18 06:58:31 +0530

    CMT

--- outputs.tf
+++ outputs.tf
@@ -2,18 +2,3 @@ output "azs" {
   value = module.roboshop.azs
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
